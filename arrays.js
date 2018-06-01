9cc6fe4207d3060e29ae1ae9ecf8e55f3e00d232
Shravani PL <pl.shravani@gmail.com>
2018-06-01 01:55:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
   
 }
+//alters the original array 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
