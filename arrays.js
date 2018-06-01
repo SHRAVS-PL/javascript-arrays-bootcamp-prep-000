ce453453e66e7bb859d44a40a80c594c612ec0cd
Shravani PL <pl.shravani@gmail.com>
2018-06-01 01:58:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,6 +24,7 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array,index){
   return array[index]
 }
+//Removes the first element and alters the array
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
